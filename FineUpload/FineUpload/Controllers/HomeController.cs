975740c4c280f12916303e68056491008716d322
Cleyton Ferrari <cleyton@w7br.com>
2013-05-20 22:28:27 -0400

    Adicionado o resizer de imagem

--- FineUpload/FineUpload/Controllers/HomeController.cs
+++ FineUpload/FineUpload/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using FineUpload.Helpers;
+using ImageResizer;
 
 namespace FineUpload.Controllers
 {
@@ -22,22 +23,23 @@ namespace FineUpload.Controllers
         [HttpPost]
         public FineUploaderResult UploadFile(FineUploads upload)
         {
-            // asp.net mvc will set extraParam1 and extraParam2 from the params object passed by Fine-Uploader
-
             var dir = System.Web.HttpContext.Current.Server.MapPath("~/Content/Uploads/");
 
-            var filePath = Path.Combine(dir, upload.Filename);
+            Dictionary<string, string> listaDeArquivos;
+
             try
             {
-                upload.SaveAs(filePath, overwrite: true, autoCreateDirectory: true);
+                listaDeArquivos = upload.SaveAsVersions(dir, upload, autoCreateDirectory: true);
+
             }
             catch (Exception ex)
             {
                 return new FineUploaderResult(false, error: ex.Message);
             }
 
-            // the anonymous object in the result below will be convert to json and set back to the browser
-            return new FineUploaderResult(true, new { img = "Content/Uploads/" + upload.Filename });
+            return new FineUploaderResult(true, new { img = "Content/Uploads/" + listaDeArquivos["_thumb"] });
         }
+
+
     }
 }
--- FineUpload/FineUpload/Helpers/FineUploads.cs
+++ FineUpload/FineUpload/Helpers/FineUploads.cs
@@ -1,5 +1,7 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Web.Mvc;
+using ImageResizer;
 
 namespace FineUpload.Helpers
 {
@@ -20,6 +22,60 @@ namespace FineUpload.Helpers
             using (var file = new FileStream(destination, overwrite ? FileMode.Create : FileMode.CreateNew))
                 InputStream.CopyTo(file);
         }
+
+
+        public Dictionary<string, string> SaveAsVersions(string destination, FineUploads upload, bool autoCreateDirectory = true)
+        {
+            var versions = new Dictionary<string, string>
+                               {
+                                   {"_thumb", "width=100&height=100&format=jpg&crop=auto"},
+                                   {"_medium", "maxwidth=400&maxheight=400format=jpg"},
+                                   {"_large", "maxwidth=1600&maxheight=1600&format=jpg"}
+                               };
+
+            if (autoCreateDirectory)
+            {
+                var directory = new FileInfo(destination).Directory;
+                if (directory != null) directory.Create();
+            }
+
+            //Isso ta muito feio, deve ter outra maneira de fazer isso, ao ler no Build, ele zera o inputStream, entao criei uma copia
+            // http://eyeung003.blogspot.com.br/2010/01/c-reading-from-closed-stream.html
+            var file = CopyStream(upload.InputStream);
+
+            var retorno = new Dictionary<string, string>();
+            var guid = System.Guid.NewGuid().ToString();
+
+            foreach (var suffix in versions.Keys)
+            {                
+                var fileName = Path.Combine(destination, guid + suffix);
+                fileName = ImageBuilder.Current.Build(file, fileName, new ResizeSettings(versions[suffix]), false, true);
+
+                retorno.Add(suffix, Path.GetFileName(fileName));
+            }
+
+            file.Flush();
+            file.Close();
+
+            return retorno;
+        }
+
+        private static Stream CopyStream(Stream inputStream)
+        {
+            const int readSize = 256;
+            byte[] buffer = new byte[readSize];
+            MemoryStream ms = new MemoryStream();
+
+            int count = inputStream.Read(buffer, 0, readSize);
+            while (count > 0)
+            {
+                ms.Write(buffer, 0, count);
+                count = inputStream.Read(buffer, 0, readSize);
+            }
+            ms.Seek(0, SeekOrigin.Begin);
+            inputStream.Seek(0, SeekOrigin.Begin);
+            return ms;
+        }  
 
         public class ModelBinder : IModelBinder
         {
