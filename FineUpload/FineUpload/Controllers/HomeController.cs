0a1761a8ca6145f27bfc2689a480f114a7843278
Cleyton Ferrari <cleyton@w7br.com>
2013-05-20 20:22:24 -0400

    exemplo fineupload

---
+++ FineUpload/FineUpload/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FineUpload.Helpers;
+
+namespace FineUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public FineUploaderResult UploadFile(FineUploads upload)
+        {
+            // asp.net mvc will set extraParam1 and extraParam2 from the params object passed by Fine-Uploader
+
+            var dir = System.Web.HttpContext.Current.Server.MapPath("~/Content/Uploads/");
+
+            var filePath = Path.Combine(dir, upload.Filename);
+            try
+            {
+                upload.SaveAs(filePath, overwrite: true, autoCreateDirectory: true);
+            }
+            catch (Exception ex)
+            {
+                return new FineUploaderResult(false, error: ex.Message);
+            }
+
+            // the anonymous object in the result below will be convert to json and set back to the browser
+            return new FineUploaderResult(true, new { img = "Content/Uploads/" + upload.Filename });
+        }
+    }
+}
